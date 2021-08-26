6371bbd66274c34219faca1b12199fce39b03638
Stephanie Falls <stephms@gmail.com>
2021-08-26 16:39:32 -0400

    aug 26 439pm

--- src/components/apps/NASA.js
+++ src/components/apps/NASA.js
@@ -1,8 +1,12 @@
 import React, {useState} from 'react';
+import { geolocated } from 'react-geolocated';
+
 const baseURL = 'https://api.nasa.gov/planetary/earth/imagery';
 const key = 'sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC';
 
 const NASA = () => {
+    const [latitude, setLatitude] = useState('');
+    const [longitude, setlongitude] = useState('');
     const [satellite, setSatellite] = useState([])
 
     const fetchSatellite = () => {
