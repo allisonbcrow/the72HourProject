6bda3a7fc5439b50cbe27f516eaa6648be9a90d2
allisonbcrow <83992047+allisonbcrow@users.noreply.github.com>
2021-08-26 11:50:49 -0400

    pushing updated branch updated sidebar and geolocation is empty

--- src/app.js
+++ src/app.js
@@ -4,22 +4,16 @@ import './app.css';
 import Header from './components/site/Header'
 import Footer from './components/site/Footer'
 import Sidebar from './components/site/Sidebar'
-import NASA from './components/apps/NASA'
-import Ticketmaster from './components/apps/Ticketmaster'
-import Weather from './components/apps/Weather'
-import Geolocation from './components/concepts/Geolocation';
+
 import {
   BrowserRouter as Router,
 } from 'react-router-dom';
 
 function App() {
+
   return (
     <div>
       <Header/>
-      <Geolocation/>
-      <NASA/>
-      <Weather/>
-      <Ticketmaster/>
       <Router>
         <Sidebar/>
         </Router>  
@@ -30,6 +24,3 @@ function App() {
 
 export default App;
 
-
-// const weatherApiKey = process.env.WEATHER_API_KEY;
-// const weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${state}&appid=${weatherApiKey}`;
