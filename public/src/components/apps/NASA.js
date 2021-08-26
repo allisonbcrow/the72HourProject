093ae67207d328a3c30950ddf9af761fe4874bab
Stephanie Falls <stephms@gmail.com>
2021-08-26 12:47:12 -0400

    Nasa setup

--- public/src/components/apps/NASA.js
+++ public/src/components/apps/NASA.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react';
+const baseURL = 'https://api.nasa.gov/planetary/earth/imagery';
+const key = 'sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC';
 
 const NASA = () => {
+    const [location, setLocation] = useState([])
+
+    const fetchLocation = () => {
+        let url = `${baseURL}?api-key=${key}`;
+
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setLocation(data.response.docs))
+    }
+
     return (
         <div>
 
