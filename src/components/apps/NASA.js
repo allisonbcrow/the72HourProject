f32cf90a38384549326057f482345bd044bf4dfb
Stephanie Falls <stephms@gmail.com>
2021-08-26 15:16:41 -0400

    update 316pm

--- src/components/apps/NASA.js
+++ src/components/apps/NASA.js
@@ -3,19 +3,28 @@ const baseURL = 'https://api.nasa.gov/planetary/earth/imagery';
 const key = 'sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC';
 
 const NASA = () => {
-    const [location, setLocation] = useState([])
+    const [satellite, setSatellite] = useState([])
 
-    const fetchLocation = () => {
+    const fetchSatellite = () => {
         let url = `${baseURL}?api-key=${key}`;
+
+        url = latitude ? url + `&latitude=${latitude}` : url;
+        url = longitude ? url + `&longitude=${longitude}` : url;
 
         fetch(url)
         .then(res => res.json())
-        .then(data => setLocation(data.response.docs))
+        .then(data => setSatellite(data.response.docs))
+    };
+
+    const showImg = () => {
+
     }
 
     return (
-        <div>
-
+        <div className="main">
+            <div className="mainDiv">
+                <img src={satellite} />
+            </div>
         </div>
     );
 };
