2b4be4242d3409e88f24d5d2d8577232e6f7e563
Stephanie Falls <stephms@gmail.com>
2021-08-28 12:24:50 -0400

    sat noonthirty

--- src/components/apps/NASA.js
+++ src/components/apps/NASA.js
@@ -1,76 +1,56 @@
-import React, {Component} from 'react';
-// import { geolocated } from 'react-geolocated';
-import {render} from "react-dom";
+import React, { useState, useEffect } from "react";
+const NASA = (props) => {
+    const [NASAImg, setNASAImg] = useState("");
+    const [lat, setLat] = useState(null);
+    const [lng, setLng] = useState(null);
+    const [status, setStatus] = useState(null);
+    const latlon = lat + lng;
+  function getNASAImg() {
+    // const baseURL =
+    //   "https://api.nasa.gov/planetary/earth/imagery/date?api_key=26NGKr0DLIKJVwuABhcPReibvZd7oV2EqeQ3xSR0";
+    // const key = '26NGKr0DLIKJVwuABhcPReibvZd7oV2EqeQ3xSR0'
 
-const baseURL = 'https://api.nasa.gov/planetary/earth/imagery/date';
-const key = 'sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC';
+    const testURL =
+      `https://api.nasa.gov/planetary/earth/imagery?lon=${lng}&lat=${lat}&date=2020-02-01&api_key=26NGKr0DLIKJVwuABhcPReibvZd7oV2EqeQ3xSR0`;
 
-class NASA extends Component {
-    
-    constructor(props) {
-        super(props);
-        this.state = { date: [] };  
- };
+    fetch(testURL).then((res) => setNASAImg(res.url));
+  }
 
+  useEffect(() => {
+      if (lat&&lng) {
 
- fetchResults = () => {
-     let url = `${baseURL}?api-key=${key}`;
-     
-     fetch(url)
-     .then(res => res.json())
-     .then(json => this.setState({ date: json.data }))
-     .catch(err => console.log(err));
- };
+          getNASAImg();
+        }
+  }, [lat,lng]);
 
- componentDidMount() {
-       navigator.geolocation.getCurrentPosition(function(position) {
-         console.log("Latitude is :", position.coords.latitude);
-         console.log("Longitude is :", position.coords.longitude);
-     });
-    };
+  const getLocation = () => {
+    if (!navigator.geolocation) {
+      setStatus("Geolocation is not supported by your browser");
+    } else {
+      setStatus("Locating...");
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setStatus(null);
+          setLat(position.coords.latitude);
+          setLng(position.coords.longitude);
+          console.log(latlon);
+        },
+        () => {
+          setStatus("Unable to retrieve your location");
+        }
+      );
+    }
+  };
 
- 
- render() {
-     return (
-         <div>
-          <img src={this.fetchResults } />
-         </div>
-     );
-   }
-}
-
-render(<NASA />, document.getElementById("root"));
+  return (
+    <div className="App">
+        <button onClick={getLocation}>Get My Location</button>
+      <img style={{ height: 190, width: 190 }} src={NASAImg} />
+      <p>{status}</p>
+      {lat && <p>Latitude: {lat}</p>}
+      {lng && <p>Longitude: {lng}</p>}
+    </div>
+  );
+};
 
 export default NASA;
-
-
-// function NASA({ coords }) {
-//     const [Intialized, setIntialized] = useState(false);
-// }
-
-// const getSatellite = async () => { 
-//     const { latitude, longitude } = coords;
-//     const { data } = await (latitude, longitude);
-//     const keyword = (
-//         data.results[0].address_components.find(c =>
-//             c.types.includes("locality")
-//             ) || {}
-//             ).long_name;
-//             localStorage.setItem("keyword", keyword);
-//             // keywordStore.setKeyword(keyword);
-            
-//             return (
-//                 <div className="main">
-//                     <div className="mainDiv">
-//                         <button type="button" onClick={getSatellite} >Get Image</button>
-//                     </div>
-//                 </div>
-//             );
-//         };
-    
-// export default geolocated({
-//     positionOptions: {
-//         enableHighAccuracy: false
-//     },
-//     userDecisionTimeout: 5000
-// }) (NASA);
